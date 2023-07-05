e54582779b734171c479fa66c033dfe257361a76
lyndonrey <lyndonreybualat@gmail.com>
2023-07-05 13:50:08 -0600

    change the footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { AiTwotonePhone, AiFillHome } from "react-icons/ai";
 import { IoMail } from "react-icons/io5";
+import { client } from "../client";
 const Footer = () => {
+  const [footer, setFooter] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "footer"]';
+    client
+      .fetch(query)
+      .then((data) => setFooter(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
     <footer>
       {/* Newsletter */}
+
       <div className="col-span-4   flex justify-center pt-10 ">
         <form action="#" className="w-5/6 md:w-2/4">
           <div className="gird-cols-3 grid gap-4 ">
@@ -46,62 +56,60 @@ const Footer = () => {
       </div>
 
       {/*  */}
-      <div className="w-5/6 md:container mx-auto py-10 text-center md:text-left">
-        <div className="flex-cols md:flex">
-          {/*  */}
-          <div className="w-full md:w-3/6">
-            <h2 className="mb-4 text-2xl flex items-center justify-center font-semibold uppercase md:justify-start">
-              Jireh Koinonia Calgary
-            </h2>
-            <p>
-              Welcome to our vibrant church community where faith comes alive.
-              We gather in love, worship with passion, and serve with
-              compassion. Join us as we grow together, find purpose, and make a
-              positive impact in the world.
-            </p>
-          </div>
-          {/*  */}
-          <div className="mx-auto mt-3">
-            <h6 className=" text-2xl flex justify-center font-semibold uppercase md:justify-start">
-              Socials
-            </h6>
-            <div className="flex text-2xl justify-center">
-              <div className="mr-2">
-                <a
-                  href="#!"
-                  className="text-neutral-200  hover:text-yellow-600">
-                  <FaInstagram />
-                </a>
-              </div>
-              <div className="">
-                <a
-                  href="#!"
-                  className="text-neutral-200  hover:text-yellow-600">
-                  <FaFacebook />
-                </a>
-              </div>
-            </div>
-          </div>
 
-          {/* Contacts */}
-          <div className="mx-auto mt-4">
-            <h6 className="text-2xl flex justify-center font-semibold uppercase md:justify-start">
-              Contact
-            </h6>
-            <div className="mb-4 flex items-center justify-center md:justify-start">
-              <AiFillHome />
-              <p className="ml-2">5510 26 Ave NE, Calgary, AB T1Y 6S1</p>
+      <div className="w-5/6 md:container mx-auto py-10 text-center md:text-left">
+        {footer.length > 0 && (
+          <div className="flex-cols h-full md:flex">
+            <div className="w-full md:w-3/6 mt-3 md:mt-0">
+              <h2 className="mb-4 text-2xl flex items-center justify-center font-semibold uppercase md:justify-start">
+                {footer[1].title}
+              </h2>
+
+              <p>{footer[1].description}</p>
             </div>
-            <div className="mb-4 flex items-center justify-center md:justify-start">
-              <IoMail />
-              <p className="ml-2">Jirehkoinoniacalgary@gmail.com</p>
+
+            <div className="mx-aut mt-3 md:mt-0">
+              <h6 className="mb-4  text-2xl flex justify-center font-semibold uppercase md:justify-start">
+                {footer[0].title}
+              </h6>
+              <div className="flex text-2xl justify-center">
+                <div className="mr-2">
+                  <a
+                    href="#!"
+                    className="text-neutral-200  hover:text-yellow-600">
+                    <FaInstagram />
+                  </a>
+                </div>
+                <div className="">
+                  <a
+                    href="#!"
+                    className="text-neutral-200  hover:text-yellow-600">
+                    <FaFacebook />
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className="mb-4 flex items-center justify-center md:justify-start">
-              <AiTwotonePhone />
-              <p className="ml-2">+ 01 234 567 88</p>
+
+            {/* Contacts */}
+            <div className="mx-auto mt-3 md:mt-0">
+              <h6 className="mb-4 text-2xl flex justify-center font-semibold uppercase md:justify-start">
+                {footer[2].title}
+              </h6>
+              <div className="mb-4 flex items-center justify-center md:justify-start">
+                <AiFillHome />
+                <p className="ml-2">{footer[2].address}</p>
+              </div>
+              <div className="mb-4 flex items-center justify-center md:justify-start">
+                <IoMail />
+                <p className="ml-2">{footer[2].email}</p>
+              </div>
+              <div className="mb-4 flex items-center justify-center md:justify-start">
+                <AiTwotonePhone />
+                <p className="ml-2">{footer[2].phone}</p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
 
       <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
