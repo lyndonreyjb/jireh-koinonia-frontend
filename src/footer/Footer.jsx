4e4dab2dadab4381994f70ebc16ca79ac73f0367
lyndonrey <lyndonreybualat@gmail.com>
2023-07-05 13:51:20 -0600

    change footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -15,7 +15,6 @@ const Footer = () => {
   return (
     <footer>
       {/* Newsletter */}
-
       <div className="col-span-4   flex justify-center pt-10 ">
         <form action="#" className="w-5/6 md:w-2/4">
           <div className="gird-cols-3 grid gap-4 ">
